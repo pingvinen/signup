049b60431e49e458ee496304bd61058774c2394b
Jess Andreas Olsen <jess@simplii.mx>
2017-01-04 14:09:09 +0100

    Made sidebar sticky and started on form logic

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,29 @@
 //= require_tree .
+
+
+
+$(function() {
+  	// Handler for .ready() called.
+
+  	$("#port-number").change(function () {
+      var value = $(this).val();
+      $("#contact-phone").val(value);
+      $("#sum-contact-phone").text(value);
+    }).keyup();
+
+    $("#contact-name").change(function () {
+      var value = $(this).val();
+      $("#delivery-name").val(value);
+      $("#credit-name").val(value);
+      $("#sum-contact-name").text(value);
+    }).keyup();
+
+    $("#contact-email").change(function () {
+      var value = $(this).val();
+      $("#sum-contact-email").text(value);
+    }).keyup();
+});
+
+$(function() {
+	$("#sidebar").sticky({topSpacing:30});
+});
