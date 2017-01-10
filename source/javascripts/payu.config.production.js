9af2a532445deb1cfb12d6de82bc2097b820c506
Patrick Boisen <patrick@skarp.dk>
2017-01-10 10:59:56 +0100

    Add production config for payU (with fake values)
    Also ensure that production builds do not include the development config and vice versa.

---
+++ source/javascripts/payu.config.production.js
@@ -0,0 +1,16 @@
+
+(function(undefined) {
+    /**
+     * PayU config for testing
+     * @type {{url: string, publicKey: string, language: string, accountId: string}}
+     */
+
+    window.payuConfig = {
+        url: 'https://api.payulatam.com/payments-api/4.0/service',
+        publicKey: 'PKaC6H4cEDJD919n705L544kSU',
+        language: 'en',
+        accountId: '666666',
+        payerId: '666'
+    };
+
+})();
